943c17dc02399ec11d03184b6f49be089680c502
Tiago Câmara <tiago.camara@itpeople.pt>
2020-06-02 20:26:52 +0100

    added pop-up for map

--- map-poi/map.js
+++ map-poi/map.js
@@ -1,13 +1,23 @@
-const inputPoi = window.document.getElementById('inputPoi');
-const mapDiv = window.document.getElementById('map-poi'); // access map component
+const inputPoi = window.document.getElementById('input-poi');
+const mapWrapper = window.document.getElementById('map-wrapper'); // access map component
+const mapDiv = window.document.getElementById('map-poi');
 
+// map toggle
 inputPoi.onclick = () => {
-  mapDiv.style.display = 'block';
+  mapWrapper.style.display = 'flex';
+};
+
+mapWrapper.onclick = () => {
+  mapWrapper.style.display = 'none';
+};
+
+mapDiv.onclick = () => {
+  event.stopPropagation();
 };
 
 // init map
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map-poi'), {
+  map = new google.maps.Map(mapDiv, {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
       });
