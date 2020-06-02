a4922f6d6590e404d047af58d03ba38b11ab52e7
Tiago Câmara <tiago.camara@itpeople.pt>
2020-06-02 20:49:55 +0100

    click on map & retrieve lat/lng

--- map-poi/map.js
+++ map-poi/map.js
@@ -15,10 +15,30 @@ mapDiv.onclick = () => {
   event.stopPropagation();
 };
 
-// init map
+// MAP functions
 function initMap() {
+  const initLatlng = {lat: -25.363, lng: 131.044};
+
   map = new google.maps.Map(mapDiv, {
-        center: {lat: -34.397, lng: 150.644},
+        center: initLatlng,
         zoom: 8
       });
+
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow(
+    {content: 'Click the map to get Lat/Lng!', position: initLatlng});
+  infoWindow.open(map);
+
+  // Configure the click listener.
+  map.addListener('click', function(mapsMouseEvent) {
+    // Close the current InfoWindow.
+    infoWindow.close();
+
+    const selectedLatLng = mapsMouseEvent.latLng;
+
+    // add values to input
+    inputPoi.value = `${selectedLatLng.lat()} ${selectedLatLng.lng()}`;
+    // close map window
+    mapWrapper.style.display = 'none';
+  });
 }
