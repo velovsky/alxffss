e2d8b942538eaa1931a772d223d870440569137d
Tiago Câmara <tiago.camara@itpeople.pt>
2020-06-03 18:50:29 +0100

    finisehd demo Map-POIs

--- map-poi/map.js
+++ map-poi/map.js
@@ -2,6 +2,7 @@ const inputPoi = window.document.getElementById('input-poi');
 const mapWrapper = window.document.getElementById('map-wrapper'); // access map component
 const mapDiv = window.document.getElementById('map-poi');
 const infoPoi = window.document.getElementById('info-poi');
+const submitBtn = mapWrapper.querySelector('.submit');
 
 // map toggle
 inputPoi.onclick = () => {
@@ -22,6 +23,11 @@ mapDiv.onclick = () => {
   event.stopPropagation();
 };
 
+submitBtn.onclick = () => {
+  event.stopPropagation();
+  closePopUp();
+};
+
 // MAP functions
 function initMap() {
   const initLatlng = {lat: 38.802210, lng: -77.043071}; // Alexandria
@@ -36,6 +42,8 @@ function initMap() {
     {content: 'Click the map to get Lat/Lng!', position: initLatlng});
   infoWindow.open(map);
 
+  let selectedMarker;
+
   // Configure the click listener.
   map.addListener('click', function(mapsMouseEvent) {
     // Close the current InfoWindow.
@@ -43,14 +51,26 @@ function initMap() {
 
     const selectedLatLng = mapsMouseEvent.latLng;
 
-    // TODO: add a marker and a confirm button
+    // remove exisitng marker, if exists
+    if (selectedMarker) {
+      selectedMarker.setMap(null);
+    }
+
+    // create new one
+    selectedMarker = new google.maps.Marker({
+      position: selectedLatLng,
+      map: map,
+      title: 'This is the selected spot'
+    });
 
     // add values to input & poi info
     inputPoi.value = `${selectedLatLng.lat()} ${selectedLatLng.lng()}`;
     infoPoi.querySelector('.lat').innerText = selectedLatLng.lat().toFixed(4);
     infoPoi.querySelector('.lng').innerText = selectedLatLng.lng().toFixed(4);
 
-    // close map window
-    closePopUp();
+    // enable submit btn
+    if (submitBtn.disabled) {
+      submitBtn.disabled = false;
+    }
   });
 }
